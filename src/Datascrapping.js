98e6d92e86a565ec23a3bb582178f1e83939bcb1
Ahmad Alif Sofian <60426463+alifahmaaad@users.noreply.github.com>
2021-04-30 21:46:46 +0700

    Update Datascrapping.js

--- src/Datascrapping.js
+++ src/Datascrapping.js
@@ -1,25 +1,47 @@
 import React, { Component } from "react";
 import cheerio from "cheerio";
-import axios from 'axios';
+import axios from "axios";
 import "./App.css";
 
-
-const divStyle = {
-  color: 'black',
-  margin: 'auto'
-};
-class DataScrapping extends Component {
-  state = { datas: [] };
+class Scraping extends Component {
+  state = { data: [] };
 
   async componentDidMount() {
-   let datas = [];
-   const htmlDetik = await axios.get("https://cors-anywhere.herokuapp.com/https://sport.detik.com/");
+    let data = [];
+
+    const htmlPandit = await axios.get("https://cors-anywhere.herokuapp.com/https://panditfootball.com/");
+    const $pandit = await cheerio.load(htmlPandit.data);
+    $pandit("article.news-block.small-block").each((i, element) => {
+      data.push({
+        title: $pandit(element).find("h3.news-title > a").text().trim(),
+        image: $pandit(element).find("a.overlay-link > figure.image-overlay > img").attr("src"),
+        date: $pandit(element).find("p.simple-share").text().trim(),
+        source: "https://panditfootball.com",
+        link: $pandit(element).find("h3.news-title > a").attr("href"),
+      });
+    });
+    this.setState({ data });
+
+    const htmlGoalCom = await axios.get("https://cors-anywhere.herokuapp.com/https://www.goal.com/id");
+    const $goalcom = await cheerio.load(htmlGoalCom.data);
+    $goalcom("div.group.card-11-group-1 > article").each((i, element) => {
+      data.push({
+        title: $goalcom(element).find("a > div.title > h3").text().trim(),
+        image: $goalcom(element).find("img").attr("srcset"),
+        date: $goalcom(element).find("footer > time").attr("datetime").substr(0, 19).replace("T", " "),
+        source: "https://goal.com",
+        link: $goalcom(element).find("a").attr("href"),
+      });
+    });
+    this.setState({ data });
+
+    const htmlDetik = await axios.get("https://cors-anywhere.herokuapp.com/https://sport.detik.com/");
     const $detik = await cheerio.load(htmlDetik.data);
     let jDetikCom = 0;
     $detik("div.m_content > ul > li > article").each((i, element) => {
       let kategori = $detik(element).find("div.desc_nhl > a").attr("href").substr(24, 9);
       if (kategori == "sepakbola" && jDetikCom <= 15) {
-        datas.push({
+        data.push({
           title: $detik(element).find("div.desc_nhl > a > h2").text().trim(),
           image: $detik(element).find("img").attr("src"),
           date: $detik(element).find("div.desc_nhl > span.labdate").text().replace("detikSport", "").replace("|", "").trim(),
@@ -29,50 +51,29 @@ class DataScrapping extends Component {
         jDetikCom++;
       }
     });
-    this.setState({ datas });
-    const html = await axios.get('https://cors-anywhere.herokuapp.com/https://www.bola.com/dunia');
-    const $ = await cheerio.load(html.data);
- 
-    $('.articles--iridescent-list > article').each((i, element) => {
-      if (i < 10) {
-        datas.push({
-          title: $(element).find('aside > header > h4').text().trim(),
-          image : $(element).find('figure > a > picture > img').attr('data-src'),
-          date: $(element).find('span.labdate').text().replace("detikSport","").replace("|", "").trim(),
-          src : "bola.com"
-        })
-      }
-    });
-    this.setState({ datas });
-    const html4 = await axios.get('https://cors-anywhere.herokuapp.com/https://www.panditfootball.com/');
-    const $3 = await cheerio.load(html4.data);
+    this.setState({ data });
 
-    $3("article.news-block.small-block").each((i, element) => {
-      if (i < 10) {
-        datas.push({
-          title: $3(element).find('h3.news-title > a').text().trim(),
-          image : $3(element).find('a.overlay-link > figure.image-overlay > img').attr('src'),
-          date: $3(element).find('p.simple-share').text().trim(),
-          src : "panditfootball.com"
-        })
+    const htmlBolaCom = await axios.get("https://cors-anywhere.herokuapp.com/https://www.bola.com");
+    const $bolacom = await cheerio.load(htmlBolaCom.data);
+    let jBolaCom = 0;
+    $bolacom("div.articles--iridescent-list > article").each((i, element) => {
+      let kategori = $bolacom(element).find("aside > header > a.articles--iridescent-list--text-item__category").text().trim();
+      let judulFoto = $bolacom(element).find("aside > header > h4").text().trim().substr(0, 5);
+      let judulVideo = $bolacom(element).find("aside > header > h4").text().trim().substr(0, 6);
+      if (jBolaCom <= 15 && kategori != "E-sports" && kategori != "MotoGP" && kategori != "NBA") {
+        if (judulFoto != "FOTO:" && judulVideo != "VIDEO:") {
+          data.push({
+            title: $bolacom(element).find("aside > header > h4").text().trim(),
+            image: $bolacom(element).find("figure > a > picture > img").attr("data-src"),
+            date: $bolacom(element).find("aside > header > span.articles--iridescent-list--text-item__datetime > time").text(),
+            source: "https://bola.com",
+            link: $bolacom(element).find("aside > header > h4 > a").attr("href"),
+          });
+          jBolaCom++;
+        }
       }
     });
-    this.setState({ datas });
-    const html2 = await axios.get('https://cors-anywhere.herokuapp.com/https://www.goal.com/id/berita/4');
-    const $4 = await cheerio.load(html2.data);
-    
-    $4('.main-content > section >').each((i, element) => {
-      if (i> 1 && i < 12) {
-        datas.push({
-          title: $4(element).find('.title > h3').text().trim(),
-          image : $4(element).find('.picture > img').prop('srcset'),
-          date: $4(element).find('footer > time').attr('datetime'),
-          src : "goal.com"
-        })
-      }
-    });
-    this.setState({ datas });
-    console.log(datas)
+    this.setState({ data });
   }
  
   render() {
